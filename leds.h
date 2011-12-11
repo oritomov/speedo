23a90debb2cf46b4873c33ffba99a90e6f539a2d
oritomov@gmail.com <oritomov@gmail.com@dfe6edd9-6a13-ddd0-20b2-ea5bf4c82ad1>
2011-12-11 14:48:07 +0000

    fix hodo leds

--- leds.h
+++ leds.h
@@ -13,17 +13,17 @@
 #define hundreds_driver			0b00000000 // y0
 #define tens_driver				0b00000100 // y1
 #define ones_driver				0b00000010 // y2
-#define trip_hundreds_driver	0b00000110 // y3
-#define trip_tens_driver		0b00001000 // y4
+#define trip_hundreds_driver	0b00001000 // y4
+#define trip_tens_driver		0b00000110 // y3
 #define trip_ones_driver		0b00001100 // y5
 #define trip_100m_driver		0b00001010 // y6
 //								      bacg
-#define distance_6_driver		0b00001001 // y2
-#define distance_5_driver		0b00000101 // y1
-#define distance_4_driver		0b00000011 // y4
+#define distance_6_driver		0b00000101 // y1
+#define distance_5_driver		0b00001001 // y2
+#define distance_4_driver		0b00001011 // y6
 #define distance_3_driver		0b00001101 // y3
-#define distance_2_driver		0b00001011 // y6
-#define distance_1_driver		0b00000111 // y5
+#define distance_2_driver		0b00000111 // y5
+#define distance_1_driver		0b00000011 // y4
 
 void init_leds(void);
 void refresh_sevenseg(void);
