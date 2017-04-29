ce9c5b18535c526c13e09f17464e36c19b18b866
oritomov <oritomov@yahoo.com>
2017-04-29 18:32:16 +0300

    fix speed

--- src/Arduino/speedo/speed.h
+++ src/Arduino/speedo/speed.h
@@ -1,19 +1,22 @@
 #ifndef speed_h
 #define speed_h
 
+// (14" * 2.54cm * 0.01 m/cm + 2 * 195mm * 0.001 m/mm * 60%) * pi => 1.85m
+// 100m / 1.85m * 8 pulses/turn = 54 turns/100m * 8 pulses/turn = 432 pulses / 100m
+//#define DEFAULT_PULSES_IN_100M      432
+// 3600 sec/h * 1000000 us/sec * 0.1 km / 128 = 2812500 us * 100m / h
+// 2812500 / 432 pulses_in_100m = 6510
+//#define DEFAULT_CALIB_FACTOR  6510
+
+// 2ms
+#define SPEEDO_BOUNCE_THRESHOLD  16
+
 #define speedometer_pin 2
 
-// how many overflows of tmr1 before a pulse is considered "stale" (invalid)
-#define PULSE_LIFETIME      7
-// 62 bigticks = 1.98ms
-#define SPEEDO_BOUNCE_THRESHOLD  62
+extern unsigned int speed;
 
 void init_speed(void);
 void interrupt_speed(void);
-void calculate_speed(boolean forceupdate);
+void calculate_speed(void);
 
-extern unsigned int pulsecount;
-extern unsigned int speed;
-
-#endif speed_h
-
+#endif
